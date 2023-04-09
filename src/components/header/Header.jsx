d66e59cd48f766b7b8dcfd8cc054b94df82d674c
mir_faisal <mirfaisal08@gmail.comm>
2023-04-09 07:40:30 +0600

    navbar position fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <nav className=" bg-blue-500 text-white relative">
       <div className="container">
-        <div className="navbar flex justify-between py-5">
+        <div className="navbar flex justify-between items-center py-5">
           <div className="brand-name">
             <Link to={"/"}>
               <h2 className="text-2xl font-bold w-[150px]">Quice Quiz</h2>
@@ -18,7 +18,7 @@ const Header = () => {
           <ul
             className={`navbar-nav pb-4 absolute left-0 w-full flex flex-col items-center bg-blue-500 gap-4 ${
               open ? "top-full" : "top-[-200px]"
-            } md:static md:w-2/6 lg:w-1/6 md:flex md:flex-row justify-between`}
+            } md:static md:w-2/6 lg:w-1/6 md:flex md:flex-row justify-between md:items-center md:pb-0`}
           >
             <NavLink to={"/topic"}>Topic</NavLink>
             <NavLink to={"/statistics"}>Statistics</NavLink>
